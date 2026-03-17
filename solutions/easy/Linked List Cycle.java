d1f17ae3a163b3dcb5285375ef7b240dcb4bfa83
Amol Nandkishor Wadwalkar <amolnw@hotmail.com>
2026-03-16 23:51:02 -0700

    Add solution for Linked List Cycle

---
+++ solutions/easy/Linked List Cycle.java
@@ -0,0 +1,27 @@
+// Title: Linked List Cycle
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/linked-list-cycle/
+
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        boolean hasCycle = false;
+
+        while (fast != null && fast.next != null) {
+            if ( fast == slow ) {
+                hasCycle = true;
+                break;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+    }
+
+        return hasCycle;
+}
